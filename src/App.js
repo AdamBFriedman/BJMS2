01b5ac9ab4dbabc6c993a7656265cd37dcdccb50
Adam Friedman <afriedman@bignerdranch.com>
2020-11-13 21:17:25 -0500

    Add images. Add routes. Add home component.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Home from "./Pages/Home";
+import { AppBar, Toolbar, CssBaseline } from "@material-ui/core";
 import "./App.css";
 
+const useStyles = makeStyles({
+  link: {
+    color: "white",
+    marginRight: "8px",
+    textDecoration: "none",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/meet">Meet Joey</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-
-        <hr />
+        <CssBaseline />
+        <AppBar position="sticky">
+          <Toolbar>
+            <Link className={classes.link} to="/">
+              Home
+            </Link>
+            <Link className={classes.link} to="/meet">
+              Meet Joey
+            </Link>
+            <Link className={classes.link} to="/services">
+              Services
+            </Link>
+            <Link className={classes.link} to="/contact">
+              Contact
+            </Link>
+          </Toolbar>
+        </AppBar>
 
         <Switch>
           <Route exact path="/">
-            <h1>Home</h1>
+            <Home />
           </Route>
           <Route path="/meet">
-            <h1>About</h1>
+            <h1>Meet Joey</h1>
           </Route>
           <Route path="/services">
-            <h1>Dashboard</h1>
+            <h1>Services</h1>
           </Route>
           <Route path="/contact">
             <h1>Contact</h1>
