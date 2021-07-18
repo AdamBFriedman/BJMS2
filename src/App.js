8b65f2e2de6bad08a653ff6e1abd5fe03eb6eb99
adambfriedman <adam.friedman5@gmail.com>
2021-07-18 16:31:50 -0700

    Style app bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createTheme } from '@material-ui/core/styles';
 import Home from './Pages/Home';
 import Meet from './Pages/Meet';
 import Services from './Pages/Services';
 import Contact from './Pages/Contact';
 import { AppBar, Toolbar, CssBaseline } from '@material-ui/core';
 import './App.css';
+const theme = createTheme();
 
 const useStyles = makeStyles({
 	link: {
-		color: 'white',
-		marginRight: '8px',
-		textDecoration: 'none',
+	  margin: theme.spacing(2),
+	  border: "2px solid #FFD700",
+	  background: "linear-gradient(to left,#bdc3c7,#2c3e50)",
+	  borderRadius: 5,
+	  fontWeight: "bold",
+	  textDecoration: "none",
+	  textTransform: "uppercase",
+	  textShadow: "0 5px 15px rgba(0,0,0,.3)",
+	  padding: theme.spacing(2, 4),
+	  letterSpacing: 1.5,
+	  color: "#FFD700",
+	  "&:hover": {
+		border: "2px solid #3cacc8",
+		transform: "translateY(20px)",
+		background: "linear-gradient(to left,#bdc3c7,#2c3e50)",
+		transition: ".3s"
+	  }
 	},
+	appbar: { background: 'transparent', boxShadow: 'none'}
 });
 
 function App() {
@@ -21,7 +37,7 @@ function App() {
 		<Router>
 			<div>
 				<CssBaseline />
-				<AppBar position="sticky">
+				<AppBar position="sticky" className={classes.appbar}>
 					<Toolbar>
 						<Link className={classes.link} to="/">
 							Home
