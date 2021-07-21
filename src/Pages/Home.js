b607731b1328343f8b6393c6bba32d1622f9c39c
adambfriedman <adam.friedman5@gmail.com>
2021-07-21 15:21:35 -0700

    Add swinging frame

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import { makeStyles, createTheme } from '@material-ui/core/styles';
 import 'font-awesome/css/font-awesome.min.css';
 import { FaFacebook, FaYoutube, FaTwitter, FaInstagram } from 'react-icons/fa';
 import suit from '../img/suit.jpg';
+import magic from '../img/magic3.jpg';
 
 const theme = createTheme();
 
@@ -36,6 +37,191 @@ const useStyles = makeStyles({
 			opacity: 1,
 		},
 	},
+	'@keyframes swing': {
+		'0%': { transform: 'rotate(3deg)' },
+		'100%': { transform: 'rotate(-3deg)' },
+	},
+	'@keyframes cycle': {
+		'0%': {
+			top: '0px',
+		},
+		'4%': {
+			top: '0px',
+		},
+		'	16%': {
+			top: '0px',
+			opacity: '1',
+			zIndex: '0',
+		},
+		'	20%': {
+			top: '325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+		'	21%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+		'	92%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+		'	96%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'100%': {
+			top: '0px',
+			opacity: '1',
+		},
+	},
+
+	'@keyframes cycle2': {
+		'0%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	16%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	20%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	24%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	36%': {
+			top: '0px',
+			opacity: '1',
+			zIndex: '0',
+		},
+		'	40%': {
+			top: '325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+		'	41%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+		'100%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+	},
+
+	'@keyframes cycle3': {
+		'0%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	36%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	40%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	44%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	56%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	60%': {
+			top: '325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+		'	61%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+		'100%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+	},
+
+	'@keyframes cycle4': {
+		'0%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	56%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	60%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	64%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	76%': {
+			top: '0px',
+			opacity: '1',
+			zIndex: '0',
+		},
+		'	80%': {
+			top: '325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+		'	81%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+		'100%': {
+			top: '-325px',
+			opacity: '0',
+			zIndex: '-1',
+		},
+	},
+
+	'@keyframes cycle5': {
+		'0%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	76%': {
+			top: '-325px',
+			opacity: '0',
+		},
+		'	80%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	84%': {
+			top: '0px',
+			opacity: '1',
+		},
+		'	96%': {
+			top: '0px',
+			opacity: '1',
+			zIndex: '0',
+		},
+		'100%': {
+			top: '325px',
+			opacity: '0',
+			zIndex: '0',
+		},
+	},
 	homeWrapper: {
 		fontFamily: 'sans-serif',
 		textAlign: 'center',
@@ -50,7 +236,7 @@ const useStyles = makeStyles({
 	centerContent: {
 		display: 'flex',
 		flexDirection: 'column',
-		justifyContent: "center",
+		justifyContent: 'center',
 		height: '100vh',
 		margin: theme.spacing(0, 4),
 	},
@@ -137,7 +323,7 @@ const useStyles = makeStyles({
 		flexDirection: 'column',
 		listStyleType: 'none',
 		margin: 0,
-		padding: 0
+		padding: 0,
 	},
 	listItem: {
 		width: '60px',
@@ -181,6 +367,70 @@ const useStyles = makeStyles({
 		marginTop: '25px',
 		marginBottom: '25px',
 	},
+	swing: {
+		// float: 'right',
+		// position: 'relative',
+		// top: '30px',
+		// right: '30px',
+		width: '325px',
+		height: '400px',
+		border: '1.5px solid #FFD700',
+		animation: '$swing ease-in-out 1s infinite alternate',
+		boxShadow: '5px 5px 10px rgba(0,0,0,0.5)',
+		backgroundImage: `url(${magic})`,
+		backgroundRepeat: 'no-repeat',
+		/* string */
+		'&:after': {
+			content: "''",
+			position: 'absolute',
+			width: '11px',
+			height: '11px',
+			border: '1px solid #FFD700',
+			top: '-6.25px',
+			left: '50%',
+			zIndex: 0,
+			borderBottom: 'none',
+			borderRight: 'none',
+			transform: 'rotate(45deg)',
+		},
+		/* nail */
+		'&:before': {
+			content: "''",
+			position: 'absolute',
+			width: '5.5px',
+			height: '5.5px',
+			top: '-14px',
+			left: '51%',
+			zIndex: 5,
+			borderRadius: '50% 50%',
+			background: '#FFD700',
+		},
+	},
+	ol: {
+		listStyle: 'none',
+		margin: theme.spacing(2, 0, 0),
+		padding: 0
+	},
+	quote: {
+		fontSize: '20px',
+		fontStyle: 'italic',
+		color: '#FFD700',
+	},
+	anim1: {
+		animation: '$cycle 15s linear infinite',
+	},
+	anim2: {
+		animation: '$cycle2 15s linear infinite',
+	},
+	anim3: {
+		animation: '$cycle3 15s linear infinite',
+	},
+	anim4: {
+		animation: '$cycle4 15s linear infinite',
+	},
+	anim5: {
+		animation: '$cycle5 15s linear infinite',
+	},
 });
 
 export const Home = () => {
@@ -188,7 +438,6 @@ export const Home = () => {
 
 	return (
 		<div className={classes.homeWrapper}>
-			
 			<div className={classes.centerContent}>
 				<ul className={classes.list}>
 					<li className={`${classes.listItem} ${classes.facebook}`}>
@@ -241,6 +490,35 @@ export const Home = () => {
 				</h1>
 
 				<h2 className={classes.animated_H2}>Family Entertainment Specialist</h2>
+				<div className={classes.centerframe}>
+					<figure className={classes.swing}>
+						<div>
+							<ol className={classes.ol}>
+								<li className={classes.anim1}>
+									<div className={classes.quote}>
+										"Joey was the runaway hit at our holiday party."
+									</div>
+								</li>
+								<li className={classes.anim2}>
+									<div className={classes.quote}>
+										"We were all blown away by his performance. He has a gift."
+									</div>
+								</li>
+								<li className={classes.anim3}>
+									<div className={classes.quote}>"I still can't get over that ending!"</div>
+								</li>
+								<li className={classes.anim4}>
+									<div className={classes.quote}>"My kids won't stop talking about his show."</div>
+								</li>
+								<li className={classes.anim5}>
+									<div className={classes.quote}>
+										"Our guests loved every moment - thank you for a stunning performance!"
+									</div>
+								</li>
+							</ol>
+						</div>
+					</figure>
+				</div>
 			</div>
 		</div>
 	);
