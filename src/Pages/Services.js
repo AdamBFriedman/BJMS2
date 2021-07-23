618001366ebf5140f3f4ba7f16732bafcb1e225e
adambfriedman <adam.friedman5@gmail.com>
2021-07-23 13:29:55 -0700

    Add background to Services

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,9 +1,33 @@
-function Services(props) {
-  return (
-    <>
-      <h1>Services</h1>
-    </>
-  );
+import { makeStyles, createTheme } from '@material-ui/core/styles';
+import smoke from '../img/smoke.jpg';
+
+const theme = createTheme();
+
+const useStyles = makeStyles({
+	servicesContainer: {
+		fontFamily: 'Roboto, sans-serif',
+		backgroundImage: `url(${smoke})`,
+    backgroundPosition: 'center',
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		height: '100vh',
+		display: 'flex',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		// [theme.breakpoints.down('md')]: {
+		// 	flexDirection: 'column',
+		// },
+	},
+});
+
+function Services() {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.servicesContainer}>
+
+		</div>
+	);
 }
 
 export default Services;
