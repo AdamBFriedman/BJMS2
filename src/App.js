c36fcf13d41f1572f950fd9f6bed32d971b9f596
adambfriedman <adam.friedman5@gmail.com>
2021-07-25 15:17:37 -0700

    Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { makeStyles, createTheme } from '@material-ui/core/styles';
+import { makeStyles, createTheme, ThemeProvider } from '@material-ui/core/styles';
 import Home from './Pages/Home';
 import Meet from './Pages/Meet';
 import Services from './Pages/Services';
@@ -136,6 +136,7 @@ function App() {
 						<SocialMedia />
 					</div>
 				</Drawer>
+				<ThemeProvider>
 				<Switch>
 					<Route exact path="/">
 						<Home />
@@ -150,6 +151,7 @@ function App() {
 						<Contact />
 					</Route>
 				</Switch>
+				</ThemeProvider>
 			</div>
 		</Router>
 	);
