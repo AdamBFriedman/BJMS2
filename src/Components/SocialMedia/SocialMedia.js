a86a0fee6b204bf58cd326a29825ee84a1771846
adambfriedman <adam.friedman5@gmail.com>
2021-08-28 16:39:22 -0700

    Add tiktok to social media

--- src/Components/SocialMedia/SocialMedia.js
+++ src/Components/SocialMedia/SocialMedia.js
@@ -1,99 +1,129 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { FaFacebook, FaYoutube, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { FaFacebook, FaYoutube, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTiktok } from "@fortawesome/free-brands-svg-icons";
 
-const useStyles = makeStyles(theme => ({
-	list: {
-		height: '100%',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'flex-end',
-		listStyleType: 'none',
-		margin: theme.spacing(0, 3),
-		padding: 0,
-		[theme.breakpoints.down('xs')]: {
-			transform: 'translateY(0)',
-			alignItems: 'center',
-			height: 'auto',
-		},
-	},
-	listItem: {
-		width: '60px',
-		height: '60px',
-		textAlign: 'center',
-		lineHeight: '60px',
-		fontSize: '35px',
-		borderRadius: '50%',
-		background: 'black',
-		position: 'relative',
-		margin: theme.spacing(3, 0),
-		'&:hover': {
-			transform: 'translateX(20px)',
-			transition: '.3s',
-		},
-		[theme.breakpoints.up('xl')]: {
-			width: '100px',
-		height: '100px',
-		lineHeight: '100px',
-		fontSize: '60px'
-		},
-		[theme.breakpoints.down('xs')]: {
-			margin: theme.spacing(2, 0),
-			'&:hover': {
-				transform: 'translateX(0)',
-				transition: '0s',
-			},
-		},
-	},
-	icon: {
-		color: '#fff',
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)',
-	},
-	facebook: {
-		background: '#4267b2',
-	},
-	linkedin: {
-		background: '#0077b5',
-	},
-	youtube: {
-		background: '#ED3833',
-	},
-	instagram: {
-		background:
-			'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)',
-	},
+const useStyles = makeStyles((theme) => ({
+  list: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    listStyleType: "none",
+    margin: theme.spacing(0, 3),
+    padding: 0,
+    [theme.breakpoints.down("xs")]: {
+      transform: "translateY(0)",
+      alignItems: "center",
+      height: "auto",
+    },
+  },
+  listItem: {
+    width: "60px",
+    height: "60px",
+    textAlign: "center",
+    lineHeight: "60px",
+    fontSize: "35px",
+    borderRadius: "50%",
+    background: "black",
+    position: "relative",
+    margin: theme.spacing(3, 0),
+    "&:hover": {
+      transform: "translateX(20px)",
+      transition: ".3s",
+    },
+    [theme.breakpoints.up("xl")]: {
+      width: "100px",
+      height: "100px",
+      lineHeight: "100px",
+      fontSize: "60px",
+    },
+    [theme.breakpoints.down("xs")]: {
+      margin: theme.spacing(2, 0),
+      "&:hover": {
+        transform: "translateX(0)",
+        transition: "0s",
+      },
+    },
+  },
+  icon: {
+    color: "#fff",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  facebook: {
+    background: "#4267b2",
+  },
+  linkedin: {
+    background: "#0077b5",
+  },
+  youtube: {
+    background: "#ED3833",
+  },
+  instagram: {
+    background:
+      "radial-gradient(circle at 30% 100%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)",
+  },
+  tiktok: {
+    background: "radial-gradient(circle at 30% 100%, #00f2ea, #ff0050)",
+  },
 }));
 export const SocialMedia = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<ul className={classes.list}>
-			<li className={`${classes.listItem} ${classes.facebook}`}>
-				<a href="https://www.facebook.com/joeydp8trow/" target="_blank" rel="noreferrer">
-					<FaFacebook className={classes.icon} />{' '}
-				</a>
-			</li>
-			<li className={`${classes.listItem} ${classes.youtube}`}>
-				<a href="https://www.youtube.com/channel/UC4ePL-afZ7oPyiW075k0AJA" target="_blank" rel="noreferrer">
-					<FaYoutube className={classes.icon} />{' '}
-				</a>
-			</li>
-			<li className={`${classes.listItem} ${classes.linkedin}`}>
-				<a href="https://www.linkedin.com/in/joseph-dipietro-aa398b171/" target="_blank" rel="noreferrer">
-					<FaLinkedin className={classes.icon} />
-				</a>
-			</li>
-			<li className={`${classes.listItem} ${classes.instagram}`}>
-				<a href="https://www.instagram.com/bigjoeymagicshowy/" target="_blank" rel="noreferrer">
-					<FaInstagram className={classes.icon} />{' '}
-				</a>
-			</li>
-		</ul>
-	);
+  return (
+    <ul className={classes.list}>
+      <li className={`${classes.listItem} ${classes.facebook}`}>
+        <a
+          href="https://www.facebook.com/joeydp8trow/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebook className={classes.icon} />{" "}
+        </a>
+      </li>
+      <li className={`${classes.listItem} ${classes.youtube}`}>
+        <a
+          href="https://www.youtube.com/channel/UC4ePL-afZ7oPyiW075k0AJA"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaYoutube className={classes.icon} />{" "}
+        </a>
+      </li>
+      <li className={`${classes.listItem} ${classes.linkedin}`}>
+        <a
+          href="https://www.linkedin.com/in/joseph-dipietro-aa398b171/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaLinkedin className={classes.icon} />
+        </a>
+      </li>
+      <li className={`${classes.listItem} ${classes.instagram}`}>
+        <a
+          href="https://www.instagram.com/bigjoeymagicshowy/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram className={classes.icon} />{" "}
+        </a>
+      </li>
+      <li className={`${classes.listItem} ${classes.tiktok}`}>
+        <a
+          href="https://www.tiktok.com/@bigjoeymagicshow?lang=en"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon className={classes.icon} icon={faTiktok} />
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default SocialMedia;
