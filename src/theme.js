b9f1fb41d8eb4003ce3837a949de054d64dc0aad
adambfriedman <adam.friedman5@gmail.com>
2021-08-28 13:05:28 -0700

    Update theme palette

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,10 @@
 import { createTheme }  from '@material-ui/core/styles'
 const customTheme = createTheme({
   palette: {
-    primary: { main: '#3cacc8', dark: '#2c3e50'},
-    secondary: { main: '#ffd700', light: '#fceabb' },
+    // primary: { main: '#3cacc8', dark: '#2c3e50'},
+    // secondary: { main: '#ffd700', light: '#fceabb' },
+    primary: { main: '#30cb02', dark: '#426fff'},
+    secondary: { main: '#810bcb', light: '#f8ee03' },
     grey: { main: '#bdc3c7'}
   },
 })
